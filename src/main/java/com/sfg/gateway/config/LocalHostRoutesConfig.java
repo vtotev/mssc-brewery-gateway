2630245123486560f8567dd2fa5a75739973f132
Valeri Totev <72451048+vtotev@users.noreply.github.com>
2024-02-16 20:47:06 +0200

    added route configuration for beer order service

--- src/main/java/com/sfg/gateway/config/LocalHostRoutesConfig.java
+++ src/main/java/com/sfg/gateway/config/LocalHostRoutesConfig.java
@@ -16,6 +16,9 @@ public class LocalHostRoutesConfig {
                 .route(r -> r.path("/api/v1/beer", "/api/v1/beer/*", "api/v1/beerUpc/*")
                         .uri("http://localhost:8080")
                         .id("beer-service"))
+                .route(r -> r.path("/api/v1/customers*", "/api/v1/customers/**")
+                        .uri("http://localhost:8081")
+                        .id("beer-order-service"))
                 .build();
     }
 
